fcd934c0c74bbe217d7f3437fbe9128d4ddfa0fb
AGENT-SRINJOY <100292962+AGENT-SRINJOY@users.noreply.github.com>
2024-01-04 19:35:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissor = document.getElementById('scissor');
+let playerscore = document.querySelector('#pl');
+let computerscore = document.querySelector('#comp');
+let text = document.querySelector('#text');
+let playervalue = 0;
+let computervalue = 0;
+let playermv;
+
+function computermv(){
+let moves = ['rock' , 'paper' , 'scissor'];
+const random = Math.floor(Math.random() * moves.length);
+return moves[random];
+}
+
+const rockMove = () =>{
+   playermv = 'rock'
+   let comp = computermv()
+   if(playermv == comp){
+    text.innerText = 'The computer also chose rock hence draw'
+   }else if(comp == 'scissor'){
+    text.innerText = 'The computer chose scissor hence you gain a point'
+    playervalue += 1
+    playerscore.innerText = `PLAYER: ${playervalue}`;
+   }else{
+    text.innerText = 'The computer chose paper hence you loose'
+    computervalue += 1;
+    computerscore.innerText = `COMPUTER: ${computervalue}`;
+   }
+}
+
+const paperMove = () =>{
+  playermv = 'paper'
+  let comp = computermv()
+  if(playermv == comp){
+   text.innerText = 'The computer also chose paper hence draw'
+  }else if(comp == 'scissor'){
+   text.innerText = 'The computer chose scissor hence you loose '
+   
+   computervalue += 1;
+   computerscore.innerText = `COMPUTER: ${computervalue}`;
+  }else{
+   text.innerText = 'The computer chose rock hence you gain a point'
+   playervalue += 1
+   playerscore.innerText = `PLAYER: ${playervalue}`;
+  }
+}
+
+const scissorMove = () =>{
+  playermv = 'scissor'
+  let comp = computermv()
+  if(playermv == comp){
+   text.innerText = 'The computer also chose scissor hence draw'
+  }else if(comp == 'rock'){
+   text.innerText = 'The computer chose rock hence you loose '
+   computervalue += 1;
+   computerscore.innerText = `COMPUTER: ${computervalue}`;
+  }else{
+   text.innerText = 'The computer chose paper hence you gain a point'
+   playervalue += 1
+   playerscore.innerText = `PLAYER: ${playervalue}`;
+  }
+}
+
+rock.onclick = rockMove;
+paper.onclick = paperMove;
+scissor.onclick = scissorMove;
+
